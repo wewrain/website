d22a13967d2fcfcdbca45905a6fd856d35be0817
wewrain <39387490+wewrain@users.noreply.github.com>
2018-05-17 22:24:07 +0100

    Add files via upload

---
+++ _vti_cnf/language-en.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|28 May 2004 15:24:58 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|DESKTOP-UH9T861\\R
+vti_modifiedby:SR|DESKTOP-UH9T861\\R
+vti_timecreated:TR|28 May 2004 15:24:58 -0000
+vti_cacheddtm:TX|28 May 2004 15:24:58 -0000
+vti_filesize:IR|1433
+vti_backlinkinfo:VX|Reel\\ Classics/Practical/New\\ folder/fruit.html Reel\\ Classics/Practical/New\\ folder/managecart.html Reel\\ Classics/Practical/New\\ folder/checkout.html
---
+++ _vti_cnf/nopcart.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|14 Jan 2010 15:35:16 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|DESKTOP-UH9T861\\R
+vti_modifiedby:SR|DESKTOP-UH9T861\\R
+vti_timecreated:TR|14 Jan 2010 15:35:16 -0000
+vti_cacheddtm:TX|14 Jan 2010 15:35:16 -0000
+vti_filesize:IR|41480
+vti_backlinkinfo:VX|Reel\\ Classics/Practical/New\\ folder/fruit.html Reel\\ Classics/Practical/New\\ folder/managecart.html Reel\\ Classics/Practical/New\\ folder/checkout.html
